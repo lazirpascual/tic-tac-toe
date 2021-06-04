2b7cd2dca7149bb17f1a7d44b9d04096bfc75bef
Lazir Pascual <lazirpascual@gmail.com>
2021-06-04 01:09:22 -0400

    add turn based functionality per player click

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,60 @@
 const boardContainer = document.querySelector('#gameboard');
 
-const gameboard = (() => {
+// gameboard object contained in a module pattern
+const gameBoard = (() => {
     let boardBlock = [];
     const boardSize = 9;
-    let turnCounter = 0;
-
     const makeBoard = () => {
         for (let board = 0; board < boardSize; board++) {
         boardBlock[board] = document.createElement('div');
         boardBlock[board].classList.add('gameboard-block');
         boardContainer.appendChild(boardBlock[board]);
+        boardBlock[board].setAttribute('data-index', board);
         }
     };
 
     return {boardBlock, makeBoard};
 })();
 
-gameboard.makeBoard();
+// displayController object contained in a module pattern
+const displayController = (() => {
+    let turnCounter = 1;
+    const decideTurn = counter => (counter % 2 == 0) ? "even" : "odd";
+
+    return {turnCounter, decideTurn};
+})();
+
+// player object container in a factory function
+const Player = (playerName, move) => {
+    const makeTurn = index => {
+        const playerMarker = document.createElement('h1');
+        playerMarker.textContent = move;
+        playerMarker.classList.add('player-move');
+        gameBoard.boardBlock[index].appendChild(playerMarker);
+        displayController.turnCounter++;
+    }
+    return {playerName, move, makeTurn};
+}
+
+// instantiate two instances of Player
+const playerOne = Player("Player One", "X");
+const playerTwo = Player("Player Two", "O");
+
+// create the board
+gameBoard.makeBoard();
+
+// make a turn based on player on button click
+boardContainer.addEventListener('click', function(e) {
+    let index = e.target.getAttribute('data-index');
+    // if the board is currently empty, make a turn
+    if (gameBoard.boardBlock[index].childNodes.length == 0) 
+    {
+        if (displayController.decideTurn(displayController.turnCounter) == "odd") {
+            playerOne.makeTurn(index);
+        } else {
+            playerTwo.makeTurn(index);
+        }
+    }
+});
+    
+        
